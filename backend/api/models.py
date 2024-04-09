055ccc4f984a3fce2ea90cdd602a9f25529ad377
Tomislav Dukez <tomdu3@ymail.com>
2024-04-09 19:14:58 +0100

    Fix: Class Note

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Node(models.Model):
+class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
