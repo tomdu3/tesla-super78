8456cb0e868d20ebd7c5975174530be389b573ab
Tomislav Dukez <tomdu3@ymail.com>
2024-04-10 19:36:21 +0100

    Update: Not found page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,8 @@
 function NotFound() {
     return (
         <div>
-            <h1>Not Found</h1>
+            <h1>404 Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
         </div>
     )
 }
