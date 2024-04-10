77ef707abffac7b0a849889dc6a2688df5d1b9bd
Tomislav Dukez <tomdu3@ymail.com>
2024-04-10 19:18:30 +0100

    Fix: Tidy up compontents

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,20 +1,21 @@
-import axios from 'axios'
+import axios from 'axios';
+import { ACCESS_TOKEN } from './constants'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-})
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token')
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
     }
-    return config
-},
-    (error) => {
-        return Promise.reject(error)
-    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 )
 
 export default api
