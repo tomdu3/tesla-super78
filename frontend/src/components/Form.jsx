fe7d1157a1a200d385c5ed8bd49826b7794ee342
Tomislav Dukez <tomdu3@ymail.com>
2024-04-11 14:46:36 +0100

    Fix: Refactor Form component

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import api from '../api';
 import {useNavigate} from 'react-router-dom';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants';
-import LoadingIndicator from './LoadingIndicator';
 import '../styles/Form.css';
+import LoadingIndicator from './LoadingIndicator';
 
 function Form({route, method}) {
     const [username, setUsername] = useState('');
@@ -35,21 +35,28 @@ function Form({route, method}) {
         }
     }
 
-    return <form onSubmit={handleSubmit} className="form-container">
-        <h1>{name}</h1>
-        <input className="form-input"
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Username" />
-        <input className="form-input"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password" />
-        {loading && <LoadingIndicator />}
-        <button className="button" type="submit">{name}</button>
-    </form>
-}
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <h1>{name}</h1>
+            <input
+                className="form-input"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <input
+                className="form-input"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+            />
+            {loading && <LoadingIndicator />}
+            <button className="form-button" type="submit">
+                {name}
+            </button>
+        </form>
+    );
 
 export default Form
