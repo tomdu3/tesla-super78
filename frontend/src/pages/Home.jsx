788d2eefc35eb7fc76d562bff7d46cce6cfcc50f
Tomislav Dukez <tomdu3@ymail.com>
2024-04-11 11:55:57 +0100

    Add create and delete notes to home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,81 @@
+import {useState, useEffect} from 'react'
+import api from '../api'
+
 function Home() {
+    const [notes, setNotes] = useState([])
+    const [content, setContent] = useState('')
+    const [title, setTitle] = useState('')
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+    const getNotes = () => {
+        api
+        .get('/api/notes/')
+        .then((res) => res.data)
+        .then((data) => {
+            setNotes(data)
+            console.log(data)
+        })
+        .catch((err) => alert(err))
+    }
+
+    const deleteNote = (id) => {
+        api
+        .delete(`/api/notes/delete/${id}/`)
+        .then((res) => {
+            if (res.status === 204) alert('Note deleted')
+            else alert('Failed to delete note')
+        getNotes()
+        })
+        .catch((err) => alert(err))
+    }
+
+    const createNote = (e) => {
+        e.preventDefault()
+        api
+        .post('/api/notes/', {
+            content, title
+        })
+        .then((res) => {
+            if (res.status === 201) alert('Note created')
+            else alert('Failed to create note')
+            getNotes()
+        })
+        .catch((err) => alert(err))
+    }
     return (
         <div>
-            <h1>Home</h1>
+            <div>
+                <h2>Notes</h2>
+            </div>
+            <h2>Create a Note</h2>
+            <form 
+                onSubmit={createNote}>
+                    <label htmlFor="title">Title:</label>
+                    <br />
+                    <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required
+                    />
+                    <br />
+                    <label htmlFor="content">Content:</label>
+                    <br />
+                    <textarea
+                        id="content"
+                        name="content"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        required
+                    />
+                    <br />
+                    <button type="submit">Create Note</button>
+            </form>
         </div>
     )
 }
